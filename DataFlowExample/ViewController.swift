e5c94246bd1d435deaa20a6c468f7c3df078f599
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2019-05-16 15:41:21 +0700

    Delegate TextField

--- DataFlowExample/ViewController.swift
+++ DataFlowExample/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fullnameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fullnameTextField.delegate = self
+        phoneTextField.delegate = self
     }
-
 
 }
 
+//delegate gunanya untuk memanipulasi behavior dari textfield
+//membuat custom behavior yang kita butuhkan
+//extension buat textfield implementasi textfield delegate
+extension ViewController: UITextFieldDelegate
+{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        //jika textfieldnya yang fullname maka nextnya ke phoneTextField
+        if textField == fullnameTextField
+        {
+            phoneTextField.becomeFirstResponder()
+        }
+        else if textField == phoneTextField
+        {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+}
